3f336f7811c0961f6e244766813f464cb8077232
Vladimir <vladimir.evstratov@list.ru>
2021-06-06 03:35:11 +0700

    Under construction

--- src/pages/under-construction/index.tsx
+++ src/pages/under-construction/index.tsx
@@ -1,13 +1,29 @@
 import MainLayout from '../../components/common/main-layout';
+import cup from '../../images/cup.png';
+import { Button } from 'react-bootstrap';
+import { menuTitles, menuUrls } from '../../constants/structures';
+import { useHistory } from 'react-router-dom';
+import styles from './index.module.css';
 
 interface UnderConstructionProps {
   pageTitle: string;
 }
 
 const UnderConstruction = ({ pageTitle }: UnderConstructionProps) => {
+  const history = useHistory();
+
   return (
     <MainLayout title={'Under construction'} pageTitle={pageTitle}>
-      <h2>The page is under construction...</h2>
+      <div className={styles.container}>
+        <img src={cup} width="144" height="144" />
+        <p className={styles.text}>The page is under construction...</p>
+        <button
+          onClick={() => history.push(menuUrls.training)}
+          className={styles.button}
+        >
+          Go to {menuTitles.training}
+        </button>
+      </div>
     </MainLayout>
   );
 };
